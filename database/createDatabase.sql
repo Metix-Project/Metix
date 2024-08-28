66a09f835cc3f57bb6066764cf95d363e438f79a
LucasLucena31 <lucas.lucena@sptech.school>
2024-08-28 13:27:12 -0300

    Scripts de Select e Inserts + atualização das tabelas

--- database/createDatabase.sql
+++ database/createDatabase.sql
@@ -34,9 +34,12 @@ create table Servidor(
 );
 create table DadosServidor(
 		macAddress varchar(17) not null,
-    dadosCpu decimal(7, 2) not null,
-    dadosMemoria decimal(7, 2) not null,
-    dadosDisco decimal(7, 2) not null,
+    CpuPorc decimal(7, 2) not null,
+    MemoriaPorc decimal(7, 2) not null,
+    DiscoPorc decimal(7, 2) not null,
+    CpuByte int not null,
+    MemoriaByte int not null,
+    DiscoByte int not null,
     tempCpu decimal(7, 2),
     tempMemoria decimal(7, 2),
     dataHora datetime not null default current_timestamp
--- monitoramento.py
+++ monitoramento.py
@@ -1,61 +1,57 @@
-# importa as bibliotecas:
-import psutil
-import time
+import psutil;
+import time;
 
 import mysql.connector
-from uuid import getnode
+from uuid import getnode as get_mac
+macAddress = hex(get_mac())[2:]
 
-# recebe o endereço MAC do computador como um número inteiro
-# transforma em um número hexadecimal como string
-# armazena a string removendo os 2 primeiros caracteres '0x', que servem para identificar um hexadecimal:
-macAddress = hex(getnode())[2:]
+mydb = mysql.connector.connect(user='metixUserInsert', 
+                              password='sptech',
+                              host='10.18.33.18',
+                              database='Metix')
 
-# conecta ao banco de dados usando um usuário com acesso limitado:
-mydb = mysql.connector.connect(
-  user='metixUserInsert', 
-  password='sptech',
-  host='10.18.33.18',
-  database='Metix'
-)
 
-# inicia um loop infinito:
-while True:
-  # faz o monitoramento em percentual:
-  print('Monitoramento:')
-  
-  # lê e exibe o uso da CPU:
-  cpu = psutil.cpu_percent()
-  print(f'  Percentual de gasto CPU: {cpu}')
+i = 1
 
-  # lê e exibe o uso da memória RAM:
-  memoria = psutil.virtual_memory().percent
-  print(f'  Percentual de gasto memoria: {memoria}')
 
-  # armazena as partições do hardware:
-  particoes = psutil.disk_partitions()
-  # guarda a primeira partição (index 0):
-  primeiraParticao = particoes[0]
-  # armazena o caminho da primeira partição (guardado na no index 1)
-  caminhoPrimeiraParticao = primeiraParticao[1]
-  # lê e exibe o uso do primeiro e único disco:
-  disco = psutil.disk_usage(caminhoPrimeiraParticao).percent
-  print(f'  Percentual de gasto de disco: {disco}')
+while i == 1:
+    time.sleep(5)
+    print('\n')
+    print('\n')
 
-  # cria um cursor da conexão para realizar uma instrução:
-  mycursor = mydb.cursor()
+    cpuP = psutil.cpu_percent()
+    print('Percentual de gasto CPU:')
+    print(cpuP)
 
-  # armazena a instrução e os valores:
-  sql = "INSERT INTO DadosServidor (macAddress, dadosCpu, dadosMemoria, dadosDisco) VALUES (%s, %s, %s, %s)"
-  val = [
-    (macAddress, cpu, memoria, disco)
-  ]
+    cpuB = psutil.cpu_times()[0] + psutil.cpu_times()[1]
+    print('Bytes gastos CPU:')
+    print(cpuB)
 
-  # executa a instrução (várias vezes por causa do loop):
-  mycursor.executemany(sql, val)
+    memoriaP = psutil.virtual_memory().percent
+    print('Percentual de gasto memoria:')
+    print(memoriaP)
 
-  # o conector mysql do python não envia automaticamente a instrução
-  # logo, o método commit envia a instrução para o servidor de banco de dados:
-  mydb.commit()
-  
-  # espera 2 segundos para iniciar o loop novamente
-  time.sleep(2)
+    memoriaB = psutil.virtual_memory().used
+    print('Bytes gasto de memoria:')
+    print(memoriaB)
+
+    discoP = psutil.disk_usage('/').percent
+    print('Percentual de gasto de disco:')
+    print(discoP)
+
+    discoB = psutil.disk_usage('/').used
+    print('Bytes gasto em Disco:')
+    print(discoB)
+
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO DadosServidor (macAddress, CpuPorc, MemoriaPorc, DiscoPorc, CpuByte, MemoriaByte, DiscoByte) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = [
+      (macAddress, cpuP, memoriaP, discoP, cpuB, memoriaB, discoB)
+    ]
+
+    mycursor.executemany(sql, val)
+
+    mydb.commit()
+
+    # print(mycursor.rowcount, "was inserted.")
