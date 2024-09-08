085393b0bcb637d555a33116e7d354c10f442ccc
LucasNagasse <lucas.nagasse@sptech.school>
2024-09-08 16:02:10 -0300

    atualização database

--- web-data-viz/src/database/createUsers.sql
+++ web-data-viz/src/database/createUsers.sql
@@ -1,15 +1,15 @@
-drop user if exists 'metixAdm'@'%';
-create user 'metixAdm'@'%' identified by 'sptech';
+-- drop user 'metixAdm'@'%';
+create user if not exists 'metixAdm'@'%' identified by 'sptech';
 grant all privileges on Metix.* to 'metixAdm'@'%';
 
-drop user if exists 'metixUser'@'%';
-create user 'metixUser'@'%' identified by 'sptech';
+-- drop user 'metixUser'@'%';
+create user if not exists 'metixUser'@'%' identified by 'sptech';
 grant insert, select on Metix.* to 'metixUser'@'%';
 
-drop user if exists 'metixUserInsert'@'%';
-create user 'metixUserInsert'@'%' identified by 'sptech';
+-- drop user 'metixUserInsert'@'%';
+create user if not exists 'metixUserInsert'@'%' identified by 'sptech';
 grant insert on Metix.* to 'metixUserInsert'@'%';
 
-drop user if exists 'metixUserSelect'@'%';
-create user 'metixUserSelect'@'%' identified by 'sptech';
+-- drop user 'metixUserSelect'@'%';
+create user if not exists 'metixUserSelect'@'%' identified by 'sptech';
 grant select on Metix.* to 'metixUserSelect'@'%';
