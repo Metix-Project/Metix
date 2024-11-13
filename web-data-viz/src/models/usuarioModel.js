1a0cd13743622b2352ec2c8629ee987c6a756d66
Fabricio-Prudente-Ferreira <160678305+Fabricio-Prudente-Ferreira@users.noreply.github.com>
2024-11-13 19:29:09 -0300

    Update usuarioModel.js

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -4,29 +4,28 @@ var database = require("../database/connection.js");
 function listar(fkEmpresa) {
   // Retorna os usuários da empresa específica
   return database.executar(
-    `SELECT idUsuario, nome, email, fkEmpresa FROM Usuario WHERE fkEmpresa = ?;`, [fkEmpresa]
+    `SELECT * FROM Usuario WHERE fkEmpresa = ${fkEmpresa};`
   );
 }
 
 // Buscar o id da Empresa com base no codigoSeguranca
 function buscarIdEmpresaPorCodigo(codigoSeguranca) {
   return database.executar(
-    `SELECT id FROM Empresa WHERE codigoSeguranca = ?;`, [codigoSeguranca]
+    `SELECT idEmpresa FROM Empresa WHERE codigoSeguranca = ${codigoSeguranca};`
   );
 }
 
 // Cadastro de usuário usando o idEmpresa obtido pelo código de segurança
 function cadastrar(nome, email, senha, tel, cpf, cargo, fkEmpresa) {
   return database.executar(
-    `INSERT INTO Usuario (nome, email, senha, telefone, cpf, cargo, fkEmpresa) VALUES (?, ?, ?, ?, ?, ?, ?);`, 
-    [nome, email, senha, tel, cpf, cargo, fkEmpresa]
+    `INSERT INTO Usuario (nome, email, senha, telefone, cpf, cargo, fkEmpresa) VALUES (${nome}, ${email}, ${senha}, ${tel}, ${cpf}, ${cargo}, ${fkEmpresa});`
   );
 }
 
 // Autenticação de usuário considerando o codigoSeguranca
 function autenticar(email, senha, codigoSeguranca) {
   return database.executar(
-    `SELECT Usuario.*, Empresa.idEmpresa, Empresa.razaoSocial FROM Usuario JOIN Empresa ON Usuario.fkEmpresa = Empresa.idEmpresa WHERE Usuario.email = ? AND Usuario.senha = ? AND Empresa.codigoSeguranca = ?;`, [email, senha, codigoSeguranca]
+    `SELECT Usuario.*, Empresa.idEmpresa, Empresa.razaoSocial FROM Usuario JOIN Empresa ON Usuario.fkEmpresa = Empresa.idEmpresa WHERE Usuario.email = ${email} AND Usuario.senha = ${senha} AND Empresa.codigoSeguranca = ${codigoSeguranca};`
   );
 }
 
@@ -35,5 +34,5 @@ module.exports = {
   listar,
   buscarIdEmpresaPorCodigo,
   cadastrar,
-  autenticar,
+  autenticar
 };
